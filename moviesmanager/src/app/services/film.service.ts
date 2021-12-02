a3c17262b1f6e081c166b1dd2d6f08e748377182
Amira Haouet <haouetamiragmail.com>
2021-12-02 14:37:44 +0100

    angular commit

--- moviesmanager/src/app/services/film.service.ts
+++ moviesmanager/src/app/services/film.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Film } from '../models/film.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
 providedIn: 'root'
 })
 export class FilmService {
-  Films : Film[]; //un tableau de Film
+  films : Film[]; 
+  film = new Film();//un tableau de Film
+
 constructor( ) {
 //this.films = FilmService.listeFilms();
-this.Films = [
+this.films = [
   { idFilm : 1, nomFilm : "Dora", prixFilm : 3000.600, dateCreation: new Date("01/14/2011")},
   { idFilm : 2, nomFilm : "mulan", prixFilm : 450, dateCreation : new Date("12/17/2010")},
   { idFilm : 3, nomFilm :"xxx", prixFilm : 900.123, dateCreation : new Date("02/20/2020")}
@@ -24,20 +27,30 @@ this.Films = [
 ];
 }*/
 listeFilms():Film[] {
-  return this.Films
+  return this.films
 }
-ajouterFilm( film: Film){
-this.Films.push(film);
+ajouterFilm( f: Film){
+this.films.push(f);
 }
 
-supprimerFilm( film: Film){
+supprimerFilm( f: Film){
   //supprimer le Film prod du tableau Films
-  const index = this.Films.indexOf(film, 0);
+  const index = this.films.indexOf(f, 0);
   if (index > -1) {
+    this.films.splice(index, 1);
   }}
 
-  consulterProduit(id:number): Film{
-    this.Films = this.Films.find(f => f.idFilm == id);
+  consulterFilm(id:number): Film
+  {
+    this.film = this.films.find(f => f.idFilm == id);
     return this.film;
     }
+   
+
+    updateFilm(f:Film)
+{
+// console.log(f);
+this.supprimerFilm(f);
+this.ajouterFilm(f);
+}
 }
--- moviesmanager/src/app/update-film/update-film.component.ts
+++ moviesmanager/src/app/update-film/update-film.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Film } from '../models/film.model';
+import { FilmService } from '../services/film.service';
 @Component({
   selector: 'app-update-film',
   templateUrl: './update-film.component.html',
@@ -7,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class UpdateFilmComponent implements OnInit {
-
-  constructor() { }
+  currentFilm = new Film();
+  constructor(private activatedRoute: ActivatedRoute,
+    private filmService: FilmService) { }
 
   ngOnInit(): void {
-  }
 
+  //  this.currentFilm=this.filmService.currentFilm((this.activatedRoute.snapshot.params.id);
+  }
+  updateProduit()
+  { //console.log(this.currentProduit);
+  this.filmService.updateProduit(this.currentFilm);
+  }
 }

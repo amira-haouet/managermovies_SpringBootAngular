1cda2dbc6439fe84b72ec862e9ca3813579bad2d
amira-haouet <haouetamira@gmail.com>
2021-12-03 20:32:46 +0100

    update app routing

--- MesFilms/src/app/app-routing.module.ts
+++ MesFilms/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddFilmComponent } from './add-film/add-film.component';
 import { FilmsComponent } from './films/films.component';
+import { UpdateFilmComponent } from './update-film/update-film.component';
 const routes: Routes = [
   { path: "films", component: FilmsComponent },
-  { path: "add-produit", component: AddFilmComponent },
-  { path: "", redirectTo: "films", pathMatch: "full" }
+  { path: "add-film", component: AddFilmComponent },
+  { path: "", redirectTo: "films", pathMatch: "full" },
+  {path: "updateFilm/:id", component: UpdateFilmComponent}
 ];
 
 @NgModule({

b2476113033c33dcda71c2ba014e1a7cdbe1ecfa
amira-haouet <haouetamira@gmail.com>
2021-12-03 19:49:50 +0100

    film componement update

--- MesFilms/src/app/films/films.component.ts
+++ MesFilms/src/app/films/films.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from '../models/film.models';
 
 @Component({
   selector: 'app-films',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./films.component.css']
 })
 export class FilmsComponent implements OnInit {
-  films: string[];
+  //  films: string[];
+  //tableau de film
+  films: Film[];
   constructor() {
 
-    this.films = ["Dora", "dark", "stars"];
+    // this.films = ["Dora", "dark", "stars"];
+    this.films = [
+
+      { idFilm: 1, titre: "Dora", prixticket: 3000.600, dateSortie: new Date("01/14/2011") },
+      { idFilm: 2, titre: "dark", prixticket: 450, dateSortie: new Date("12/17/2010") },
+      {
+        idFilm: 3, titre: "stars", prixticket: 900.123, dateSortie: new Date("02/20/2020")
+      }
+    ];
+    
+   
   }
 
   ngOnInit(): void {

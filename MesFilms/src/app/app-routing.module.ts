3ddf1d436a002d19f30a6bac50e0d7a0161cb5bc
amira-haouet <haouetamira@gmail.com>
2021-12-03 17:57:38 +0100

    angular routing

--- MesFilms/src/app/app-routing.module.ts
+++ MesFilms/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { FilmsComponent } from './films/films.component';
+const routes: Routes = [
+  { path: "films", component: FilmsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

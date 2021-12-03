89546b6212e21f8e9d0d9e72bc0a709e469aca05
amira-haouet <haouetamira@gmail.com>
2021-12-03 18:07:16 +0100

    angular commit

--- MesFilms/src/app/app-routing.module.ts
+++ MesFilms/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddFilmComponent } from './add-film/add-film.component';
 import { FilmsComponent } from './films/films.component';
 const routes: Routes = [
-  { path: "films", component: FilmsComponent }
+  { path: "films", component: FilmsComponent },
+  {path: "add-produit", component : AddFilmComponent}
 ];
 
 @NgModule({

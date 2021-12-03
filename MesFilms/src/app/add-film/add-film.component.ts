cf74db57f463b993deebe935257eae8814e31b84
amira-haouet <haouetamira@gmail.com>
2021-12-03 19:56:41 +0100

    update addfilm

--- MesFilms/src/app/add-film/add-film.component.ts
+++ MesFilms/src/app/add-film/add-film.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Film } from '../models/film.models';
 
 @Component({
   selector: 'app-add-film',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFilmComponent implements OnInit {
 
+  newFilm = new Film();
   constructor() { }
 
   ngOnInit(): void {
--- MesFilms/src/app/app.module.ts
+++ MesFilms/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmsComponent } from './films/films.component';
 import { AddFilmComponent } from './add-film/add-film.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AddFilmComponent } from './add-film/add-film.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

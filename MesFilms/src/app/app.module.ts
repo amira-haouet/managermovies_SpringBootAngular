9cdb6dfdd89ca2ae0595eaecf677fb8860ae36e0
amira-haouet <haouetamira@gmail.com>
2021-12-03 18:06:14 +0100

    angular commit

--- MesFilms/src/app/app.module.ts
+++ MesFilms/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilmsComponent } from './films/films.component';
+import { AddFilmComponent } from './add-film/add-film.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilmsComponent
+    FilmsComponent,
+    AddFilmComponent
   ],
   imports: [
     BrowserModule,

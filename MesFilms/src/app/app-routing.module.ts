7484122b54a818abeb50953bd938def11b3f87aa
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-23 19:05:59 +0100

    add produit details in app routing

--- MesFilms/src/app/app-routing.module.ts
+++ MesFilms/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AddFilmComponent } from './add-film/add-film.component';
 import { FilmsComponent } from './films/films.component';
 import { ScenaristeComponent } from './scenariste/scenariste.component';
 import { UpdateFilmComponent } from './update-film/update-film.component';
+import {ProduitDetailsComponent} from './produit-details/produit-details.component';
 const routes: Routes = [
   { path: "films", component: FilmsComponent },
   { path: "add-film", component: AddFilmComponent },
   { path: "", redirectTo: "films", pathMatch: "full" },
   { path: "updateFilm/:id", component: UpdateFilmComponent },
-  { path: "scenariste", component: ScenaristeComponent }
+  { path: "scenariste", component: ScenaristeComponent },
+  { path: "produit-details", component: ProduitDetailsComponent },
 ];
 
 @NgModule({

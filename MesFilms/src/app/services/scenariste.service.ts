a46aa3f60060d56afb4e26b36a3042f2b2cdebd8
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-25 15:33:02 +0100

    update angular

--- MesFilms/src/app/services/scenariste.service.ts
+++ MesFilms/src/app/services/scenariste.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { Scenariste } from '../models/scenariste.models';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class ScenaristeService {
 
-  constructor() { }
+  constructor(
+
+  ) {
+
+
+  }
 }

49f19fa5181a0469cc9330d3587c637aaf57f9d4
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-26 17:11:02 +0100

    update front end

--- MesFilms/src/app/add-scenariste/add-scenariste.component.ts
+++ MesFilms/src/app/add-scenariste/add-scenariste.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Scenariste } from '../models/scenariste.models';
+import { ScenaristeService } from '../services/scenariste.service';
 
 @Component({
   selector: 'app-add-scenariste',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddScenaristeComponent implements OnInit {
 
-  constructor() { }
+  newScenariste = new Scenariste();
+  message!: string;
+  constructor(private scenaristeService: ScenaristeService,
+    private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+  //26122021 add catego(sc)
+   // this.scenaristes = this.filmService.listeScenaristes();
+//
+
+
+
   }
 
+
+  addScenariste() {
+
+    // this.newScenariste = this.filmService.consulterScenariste(this.newIdSc);
+    // this.newFilm.scenariste = this.newScenariste;
+     this.scenaristeService.ajouterScenariste(this.newScenariste).subscribe(f => {
+ 
+       console.log(f);
+     });
+     this.router.navigate(['scenaristes']).then(() => {
+ 
+       window.location.reload();
+     });
+   }
 }

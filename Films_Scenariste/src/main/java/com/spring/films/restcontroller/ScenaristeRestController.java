3698613b8a9b74b19f5f5bddcd61079e29bfe23f
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-26 08:37:27 +0100

    update backkend

--- Films_Scenariste/src/main/java/com/spring/films/restcontroller/ScenaristeRestController.java
+++ Films_Scenariste/src/main/java/com/spring/films/restcontroller/ScenaristeRestController.java
@@ -25,24 +25,31 @@ public class ScenaristeRestController {
 
 		return scenaristeService.getAllScenaristes();
 	}
+
 }
 
-/*
- * @RequestMapping(value = "/{id}", method = RequestMethod.GET) public Film
- * getFilmById(@PathVariable("id") Long id) { return filmService.getFilm(id); }
- * 
- * @RequestMapping(method = RequestMethod.POST) public Film
- * createFilm(@RequestBody Film film) { return filmService.saveFilm(film); }
- * 
- * @RequestMapping(method = RequestMethod.PUT) public Film
- * updateProduit(@RequestBody Film film) { return filmService.updateFilm(film);
- * }
- * 
- * @RequestMapping(value = "/{id}", method = RequestMethod.DELETE) public void
- * deleteFilm(@PathVariable("id") Long id) { filmService.deleteFilmById(id); }
- * 
- * @RequestMapping(path = "all", method = RequestMethod.GET) public List<Film>
- * getAllFilms1() { return filmService.getAllFilms(); }
- * 
- * }
- */
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)public Film
+
+	getFilmById(@PathVariable("id") Long id) { return filmService.getFilm(id); }
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Film createFilm(@RequestBody Film film) {
+		return filmService.saveFilm(film);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public Film updateProduit(@RequestBody Film film) {
+		return filmService.updateFilm(film);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deleteFilm(@PathVariable("id") Long id) {
+		filmService.deleteFilmById(id);
+	}
+
+	@RequestMapping(path = "all", method = RequestMethod.GET)
+	public List<Film> getAllFilms1() {
+		return filmService.getAllFilms();
+	}
+
+}

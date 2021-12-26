1c26d613095c2c39b65272ef12b4792ad9789a00
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-26 20:23:16 +0100

    update front end

--- MesFilms/src/app/add-film/add-film.component.ts
+++ MesFilms/src/app/add-film/add-film.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Film } from '../models/film.models';
 import { Scenariste } from '../models/scenariste.models';
 import { FilmService } from '../services/film.service';
+import { ScenaristeService } from '../services/scenariste.service';
 
 @Component({
   selector: 'app-add-film',
@@ -12,20 +13,20 @@ import { FilmService } from '../services/film.service';
 export class AddFilmComponent implements OnInit {
 
   //26122021 add catego(sc)
-  scenaristes! : Scenariste[];
-  newIdSc! : number;
-   newScenariste! : Scenariste;
+  scenaristes!: Scenariste[];
+  newIdSc!: number;
+  newScenariste!: Scenariste;
 
 
   newFilm = new Film();
   message!: string;
-  constructor(private filmService: FilmService,
+  constructor(private filmService: FilmService, private scenaristeservice: ScenaristeService,
     private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
 
     //26122021 add catego(sc)
-  // this.scenaristes = this.scenaristeservice.listeScenaristes();
+    this.scenaristes = this.scenaristeservice.listeScenaristes();
     //
   }
   /* addFilm() {
@@ -34,8 +35,8 @@ export class AddFilmComponent implements OnInit {
    }*/
   addFilm() {
 
-    // this.newScenariste = this.filmService.consulterScenariste(this.newIdSc);
-    // this.newFilm.scenariste = this.newScenariste;
+   // this.newScenariste = this.scenaristeservice.consulterScenariste(this.newIdSc);
+    this.newFilm.scenariste = this.newScenariste;
     this.filmService.ajouterFilm(this.newFilm).subscribe(f => {
 
       console.log(f);

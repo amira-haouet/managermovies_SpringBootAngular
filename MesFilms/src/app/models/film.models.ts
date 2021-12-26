68fc6d50a15c519840dba51f94a9fc773de462f5
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-26 11:34:04 +0100

    update front end

--- MesFilms/src/app/models/film.models.ts
+++ MesFilms/src/app/models/film.models.ts
@@ -1,6 +1,9 @@
+import { Scenariste } from "./scenariste.models";
+
 export class Film {
     idFilm!: number;
     titre!: string;
     prixticket!: number;
     dateSortie!: Date;
+    scenariste!: Scenariste;
 }

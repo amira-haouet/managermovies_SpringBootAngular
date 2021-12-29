c07ef56a5c66eaf15fa50d16c2e333ba08961f4b
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-29 21:30:06 +0100

    update project

--- MesFilms/src/app/add-film/add-film.component.ts
+++ MesFilms/src/app/add-film/add-film.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Film } from '../models/film.models';
 import { Scenariste } from '../models/scenariste.models';
@@ -12,34 +13,68 @@ import { ScenaristeService } from '../services/scenariste.service';
 })
 export class AddFilmComponent implements OnInit {
 
+  addForm : FormGroup ;
   //26122021 add catego(sc)
   scenaristes!: Scenariste[];
   newIdSc!: number;
   newScenariste!: Scenariste;
+ scenaristeService!: ScenaristeService;
 
 
   newFilm = new Film();
   message!: string;
   constructor(private filmService: FilmService, private scenaristeservice: ScenaristeService,
     private router: Router, private activatedRoute: ActivatedRoute) { }
+    scenariste!:Array<any>;
 
   ngOnInit(): void {
 
     //26122021 add catego(sc)
-    this.scenaristes = this.scenaristeservice.listeScenaristes();
+  // this.scenaristes = this.scenaristeservice.listeScenaristes();
     //
+
+    this.listeScenaristes();
+
+
   }
+
+
+  listeScenaristes()
+  {
+    
+    this.scenaristeService.listeScenariste().subscribe(
+      data=>{
+          console.log(data)
+          this.scenariste=data;
+        
+      }
+    )
+   }
   /* addFilm() {
      //console.log(this.newFilm);
      this.filmService.ajouterFilm(this.newFilm);
    }*/
   addFilm() {
+ var data=this.addForm.value;
+ var sc={
 
+  idSc:data.nom
+ }
+
+
+ console.log(data.date)
+ var m ={
+  titre : data.titre , 
+  prixticket : data.prixticket ,
+  dateSortie: data.dateSortie,
+  sexe : data.sexe , 
+  scenariste :sc
+ }
    // this.newScenariste = this.scenaristeservice.consulterScenariste(this.newIdSc);
-    this.newFilm.scenariste = this.newScenariste;
-    this.filmService.ajouterFilm(this.newFilm).subscribe(f => {
+  //  this.newFilm.scenariste = this.newScenariste;
+   // this.filmService.ajouterFilm(this.newFilm).subscribe(f => {
 
-      console.log(f);
+     //26122021 console.log(f);
     });
     this.router.navigate(['films']).then(() => {
 

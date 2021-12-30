5bec81d63badfb9ef671c3171132f0cc35816642
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-30 11:12:36 +0100

    update project

--- Backend/doctor-management/src/test/java/com/example/demo/MoviesManagementApplicationTests.java
+++ Backend/doctor-management/src/test/java/com/example/demo/MoviesManagementApplicationTests.java
@@ -25,46 +25,31 @@ class MoviesManagementApplicationTests {
 	void contextLoads() {
 	}
 
-	 @Test
-	
-	  void testAddMedecin() { Movie m = new Movie("nom2", "prenom2", new
-	  Date()); medecinRepository.save(m); }
-	
-/*
 	@Test
-	void testFindMedecin() {
-		Movie m = medecinRepository.findById(1L).get();
-		System.out.println(m);
 
-	}
-
-	@Test
-	void testFindAllMedecin() {
-		ArrayList<Movie> m = new ArrayList<>(medecinRepository.findAll());
-		for (Movie medecin : m) {
-			System.out.println(medecin);
-		}
-	}
-
-	@Test
-	void updateMedecin() {
-		Movie m = medecinRepository.findById(1L).get();
-		m.setNomMedecin("Jacque");
+	void testAddMedecin() {
+		Movie m = new Movie("dora", 1200, new Date(), "family");
 		medecinRepository.save(m);
 	}
 
-	@Test
-	void deleteMedecin() {
-		Movie m = medecinRepository.findById(1L).get();
-		medecinRepository.delete(m);
-	}
-
-	@Test
-	public void findByCategorieNomSpec() {
-		List<Movie> med = medecinRepository.findBySpecialiteNomSpec("ds");
-		for (Movie m : med) {
-			System.out.println(m);
-		}
-	}
-*/
+	/*
+	 * @Test void testFindMedecin() { Movie m =
+	 * medecinRepository.findById(1L).get(); System.out.println(m);
+	 * 
+	 * }
+	 * 
+	 * @Test void testFindAllMedecin() { ArrayList<Movie> m = new
+	 * ArrayList<>(medecinRepository.findAll()); for (Movie medecin : m) {
+	 * System.out.println(medecin); } }
+	 * 
+	 * @Test void updateMedecin() { Movie m = medecinRepository.findById(1L).get();
+	 * m.setNomMedecin("Jacque"); medecinRepository.save(m); }
+	 * 
+	 * @Test void deleteMedecin() { Movie m = medecinRepository.findById(1L).get();
+	 * medecinRepository.delete(m); }
+	 * 
+	 * @Test public void findByCategorieNomSpec() { List<Movie> med =
+	 * medecinRepository.findBySpecialiteNomSpec("ds"); for (Movie m : med) {
+	 * System.out.println(m); } }
+	 */
 }

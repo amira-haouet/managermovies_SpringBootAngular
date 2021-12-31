87ea88b0ab522d8dd7fa8527193e32eb757d166b
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-31 17:25:33 +0100

    update project

--- Frontend/movie-management/src/app/login/login.component.ts
+++ Frontend/movie-management/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from '../model/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +11,30 @@ import { User } from '../model/user';
 })
 export class LoginComponent implements OnInit {
   user = new User();
+  erreur = 0;
 
-  constructor() { }
 
+  /* constructor() { }
+ 
+   ngOnInit(): void {
+   }
+   onLoggedin() {
+     console.log(this.user);
+   }*/
+
+  constructor(private authService: AuthService,
+    private router: Router) { }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
   onLoggedin() {
     console.log(this.user);
+    let isValidUser: Boolean = this.authService.SignIn(this.user);
+    if (isValidUser)
+      this.router.navigate(['/']);
+    else
+      this.erreur = 1;
+    alert('Login ou mot de passe incorrecte!');
   }
 }
--- Frontend/movie-management/src/app/services/auth.service.ts
+++ Frontend/movie-management/src/app/services/auth.service.ts
@@ -7,60 +7,60 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
 
-  users: User[] = [{"username":"admin","password":"123","roles":['ADMIN']},
-                   {"username":"nadhem","password":"123","roles":['USER']} ];
+  users: User[] = [{ "username": "admin", "password": "123", "roles": ['ADMIN'] },
+  { "username": "nadhem", "password": "123", "roles": ['USER'] }];
 
-  public loggedUser:string;
+  public loggedUser: string;
   public isloggedIn: Boolean = false;
-  public roles:string[];
+  public roles: string[];
 
-   
+
   constructor(private router: Router) { }
 
 
 
-logout() { 
-  this.isloggedIn= false;
-  this.loggedUser = undefined;
-  this.roles = undefined;
-  localStorage.removeItem('loggedUser');
-  localStorage.setItem('isloggedIn',String(this.isloggedIn));
-  this.router.navigate(['/login']);
-}
+  logout() {
+    this.isloggedIn = false;
+    this.loggedUser = undefined;
+    this.roles = undefined;
+    localStorage.removeItem('loggedUser');
+    localStorage.setItem('isloggedIn', String(this.isloggedIn));
+    this.router.navigate(['/login']);
+  }
 
-  SignIn(user :User):Boolean{
+  SignIn(user: User): Boolean {
     let validUser: Boolean = false;
     this.users.forEach((curUser) => {
-      if(user.username=== curUser.username && user.password==curUser.password) {
+      if (user.username === curUser.username && user.password == curUser.password) {
         validUser = true;
         this.loggedUser = curUser.username;
         this.isloggedIn = true;
         this.roles = curUser.roles;
-        localStorage.setItem('loggedUser',this.loggedUser);
-        localStorage.setItem('isloggedIn',String(this.isloggedIn));
+        localStorage.setItem('loggedUser', this.loggedUser);
+        localStorage.setItem('isloggedIn', String(this.isloggedIn));
       }
     });
 
-     return validUser;
+    return validUser;
   }
 
-  isAdmin():Boolean{
+  isAdmin(): Boolean {
     if (!this.roles) //this.roles== undefiened
-       return false;
-    return (this.roles.indexOf('ADMIN') >-1);
-   
+      return false;
+    return (this.roles.indexOf('ADMIN') > -1);
+
   }
 
-  setLoggedUserFromLocalStorage(login : string) {
+  setLoggedUserFromLocalStorage(login: string) {
     this.loggedUser = login;
     this.isloggedIn = true;
     this.getUserRoles(login);
   }
 
-  getUserRoles(username :string){    
+  getUserRoles(username: string) {
     this.users.forEach((curUser) => {
-      if( curUser.username == username) {
-          this.roles = curUser.roles;
+      if (curUser.username == username) {
+        this.roles = curUser.roles;
       }
     });
   }

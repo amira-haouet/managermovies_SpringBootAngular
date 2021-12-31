74fdce5c9bc230ac8ba7de9bc72b12cdd2b0068f
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-31 15:09:20 +0100

    update project

--- Frontend/movie-management/src/app/app-routing.module.ts
+++ Frontend/movie-management/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { EditSpecComponent } from './edit-spec/edit-spec.component';
 import { EditComponent } from './edit/edit.component';
 import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie/movie.component';
+import { RechercheParScenaristeComponent } from './recherche-par-scenariste/recherche-par-scenariste.component';
 import { ScenaristeComponent } from './scenariste/scenariste.component';
 
 const routes: Routes = [
@@ -16,7 +17,7 @@ const routes: Routes = [
   { path: 'scenariste', component: ScenaristeComponent },
   { path: 'scenariste/edit', component: EditSpecComponent },
   { path: 'scenariste/add', component: AddSpecComponent },
-
+  { path: 'recherche-par-scenariste', component: RechercheParScenaristeComponent }
 ];
 
 @NgModule({

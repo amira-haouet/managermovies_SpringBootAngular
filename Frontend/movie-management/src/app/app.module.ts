d6a21ec8f77c33379c529cb9c055606acec6d79a
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2021-12-31 17:19:11 +0100

    update project

--- Frontend/movie-management/src/app/app.module.ts
+++ Frontend/movie-management/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http'
-import { ApiService } from './api.service';
+import { ApiService } from './services/api.service';
 import { MovieComponent } from './movie/movie.component';
 import { AddComponent } from './add/add.component';
 import { FormsModule } from '@angular/forms'
--- Frontend/movie-management/src/app/movie/movie.component.ts
+++ Frontend/movie-management/src/app/movie/movie.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { ApiService } from '../services/api.service';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
 @Component({
   selector: 'app-medecin',

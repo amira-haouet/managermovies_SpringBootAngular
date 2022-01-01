9376f9e8964c8781ea0b0b0dc04a4797a77e841e
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-01 10:59:25 +0100

    update project

--- Frontend/movie-management/src/app/movie/movie.component.ts
+++ Frontend/movie-management/src/app/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-medecin',
   templateUrl: './movie.component.html',
@@ -102,5 +103,5 @@ export class MovieComponent implements OnInit {
     this.key = key;
     this.reverse = !this.reverse;
   }
-
+//authService: AuthService;
 }
--- Frontend/movie-management/src/app/navbar/navbar.component.ts
+++ Frontend/movie-management/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Component({
@@ -7,10 +8,18 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  router: Router;
   constructor(public authService: AuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    let isloggedin: string;
+    let loggedUser: string;
+    isloggedin = localStorage.getItem('isloggedIn');
+    loggedUser = localStorage.getItem('loggedUser');
+    if (isloggedin != "true" || !loggedUser)
+      this.router.navigate(['/login']);
+    else
+      this.authService.setLoggedUserFromLocalStorage(loggedUser);
   }
 
 }

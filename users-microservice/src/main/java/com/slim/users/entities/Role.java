4849afb0557ff0a2c688350529712ef668e579cd
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-03 18:37:11 +0100

    update project

--- users-microservice/src/main/java/com/slim/users/entities/Role.java
+++ users-microservice/src/main/java/com/slim/users/entities/Role.java
@@ -15,4 +15,18 @@ public class Role {
 @GeneratedValue (strategy=GenerationType.IDENTITY)
 private Long role_id;
 private String role;
+public Long getRole_id() {
+	return role_id;
+}
+public void setRole_id(Long role_id) {
+	this.role_id = role_id;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+
+
 }

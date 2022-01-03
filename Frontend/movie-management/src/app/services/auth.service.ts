44d0ea0d892b47d487dde3d867c47db761fca007
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-03 20:09:51 +0100

    update project

--- Frontend/movie-management/src/app/services/auth.service.ts
+++ Frontend/movie-management/src/app/services/auth.service.ts
@@ -10,12 +10,28 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AuthService {
 
-  users: User[] = [{ "username": "admin", "password": "123", "roles": ['ADMIN'] },
-  { "username": "amira", "password": "123", "roles": ['USER'] }];
+  /* users: User[] = [{ "username": "admin", "password": "123", "roles": ['ADMIN'] },
+   { "username": "amira", "password": "123", "roles": ['USER'] }];
+  */
+
   public loggedUser: string;
   public isloggedIn: Boolean = false;
   public roles: string[];
-  constructor(private router: Router) { }
+  apiURL: string = 'http://localhost:8081/users';
+  token: string;
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+
+  login(user: User) {
+    return this.http.post<User>(this.apiURL + '/login', user, { observe: 'response' });
+  }
+  saveToken(jwt: string) {
+    localStorage.setItem('jwt', jwt);
+    this.token = jwt;
+    this.isloggedIn = true;
+  }
+
   logout() {
     this.isloggedIn = false;
     this.loggedUser = undefined;
@@ -24,20 +40,31 @@ export class AuthService {
     localStorage.setItem('isloggedIn', String(this.isloggedIn));
     this.router.navigate(['/login']);
   }
-  SignIn(user: User): Boolean {
-    let validUser: Boolean = false;
-    this.users.forEach((curUser) => {
-      if (user.username === curUser.username && user.password == curUser.password) {
-        validUser = true;
-        this.loggedUser = curUser.username;
-        this.isloggedIn = true;
-        this.roles = curUser.roles;
-        localStorage.setItem('loggedUser', this.loggedUser);
-        localStorage.setItem('isloggedIn', String(this.isloggedIn));
-      }
-    });
-    return validUser;
+
+  /**/
+  loadToken() {
+    this.token = localStorage.getItem('jwt');
   }
+  getToken(): string {
+    return this.token;
+  }
+  /*
+    SignIn(user: User): Boolean {
+      let validUser: Boolean = false;
+      this.users.forEach((curUser) => {
+        if (user.username === curUser.username && user.password == curUser.password) {
+          validUser = true;
+          this.loggedUser = curUser.username;
+          this.isloggedIn = true;
+          this.roles = curUser.roles;
+          localStorage.setItem('loggedUser', this.loggedUser);
+          localStorage.setItem('isloggedIn', String(this.isloggedIn));
+        }
+      });
+      return validUser;
+    }
+  */
+
   isAdmin(): Boolean {
     if (!this.roles) //this.roles== undefiened
       return false;
@@ -48,13 +75,15 @@ export class AuthService {
   setLoggedUserFromLocalStorage(login: string) {
     this.loggedUser = login;
     this.isloggedIn = true;
-    this.getUserRoles(login);
+    // this.getUserRoles(login);
   }
+
+  /*
   getUserRoles(username: string) {
     this.users.forEach((curUser) => {
       if (curUser.username == username) {
         this.roles = curUser.roles;
       }
     });
-  }
+  }*/
 }

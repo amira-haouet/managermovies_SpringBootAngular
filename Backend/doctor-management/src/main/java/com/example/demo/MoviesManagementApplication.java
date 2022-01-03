d6cbf4d4c9cc44065755e4431a2a45f7aef1310c
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-03 20:03:57 +0100

    update project

--- Backend/doctor-management/src/main/java/com/example/demo/MoviesManagementApplication.java
+++ Backend/doctor-management/src/main/java/com/example/demo/MoviesManagementApplication.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class MoviesManagementApplication {
+	@Autowired
+	private RepositoryRestConfiguration repositoryRestConfiguration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesManagementApplication.class, args);

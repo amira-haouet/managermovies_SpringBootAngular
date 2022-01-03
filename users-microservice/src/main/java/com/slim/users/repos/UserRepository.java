5013846caabf55d4b082f91c4cd271ac60217851
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-03 18:35:08 +0100

    update project

---
+++ users-microservice/src/main/java/com/slim/users/repos/UserRepository.java
@@ -0,0 +1,7 @@
+package com.slim.users.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.slim.users.entities.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+User findByUsername(String username);
+}

c54705408b08222c4712d63dbd74e9e839822cf0
Amira Haouet <73131244+amira-haouet@users.noreply.github.com>
2022-01-03 20:13:01 +0100

    update project

--- users-microservice/src/main/java/com/amira/users/service/UserService.java
+++ users-microservice/src/main/java/com/amira/users/service/UserService.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import com.amira.users.entities.Role;
 import com.amira.users.entities.User;
-public interface UserService { User saveUser(User user);
-User findUserByUsername (String username);
-Role addRole(Role role);
-User addRoleToUser(String username, String rolename);
-List<User> findAllUsers();
+
+public interface UserService {
+	User saveUser(User user);
+
+	User findUserByUsername(String username);
+
+	Role addRole(Role role);
+
+	User addRoleToUser(String username, String rolename);
+
+	List<User> findAllUsers();
 }
